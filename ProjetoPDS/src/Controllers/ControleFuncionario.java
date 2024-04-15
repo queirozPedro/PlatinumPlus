8064a4fe257401f0959092baefd6ee65c3f94670
mM-b <gtamsue@gmail.com>
2024-04-15 16:44:18 -0300

    implementação inicial de funcionário

--- ProjetoPDS/src/Controllers/ControleFuncionario.java
+++ ProjetoPDS/src/Controllers/ControleFuncionario.java
@@ -1,5 +1,34 @@
 package Controllers;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ControleFuncionario {
+
+
+    public void criaFuncionario() {
+        Connection connection = PostgreSQLConnection.getInstance().getConnection();
+        PreparedStatement state = null;
+        try {
+            String query = "INSERT INTO funcionario (cpf) VALUES (?)";
+            state = connection.prepareStatement(query);
+            state.setString(1, cpf);
+            state.executeUpdate();
+            System.out.println("criado funcionario");
+        } catch (Exception e) {
+            System.out.println(e);
+
+        } finally {
+            try {
+                if (state != null) {
+                    state.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     
 }
--- ProjetoPDS/src/Models/Funcionario.java
+++ ProjetoPDS/src/Models/Funcionario.java
@@ -1,9 +1,16 @@
 package Models;
 
+
 public class Funcionario extends Usuario {
+    private int idUsuario;
+
+    
+    public int getidUsuario() {
+        return idUsuario;
+    }
 
     public Funcionario(String nome, String cpf, String senha, String email, String telefone) {
         super(nome, cpf, senha, email, telefone);
     }
-    
+
 }
