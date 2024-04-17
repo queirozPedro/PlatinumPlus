3eb563c26878a7c6269d0f9cff056b7ba3f20e8c
mM-b <gtamsue@gmail.com>
2024-04-17 15:38:26 -0300

    feat: criação do metodo calculaDesconto de ControleCupom.

--- PlatinumPlus/src/Controllers/ControleCupom.java
+++ PlatinumPlus/src/Controllers/ControleCupom.java
@@ -5,11 +5,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import Models.Cupom;
+import Models.*;
 
 public class ControleCupom {
 
-    public static void criarCupom(Connection connection, int idCupom, String cpf, int desconto) {
+    public static void criarCupom(Connection connection, int idCupom, String cpf) {
         PreparedStatement state = null;
 
         if (buscaCupom(connection, idCupom) == null) {
@@ -20,7 +20,7 @@ public class ControleCupom {
                 state = connection.prepareStatement(query);
                 state.setInt(1, idCupom);
                 state.setString(2, cpf);
-                state.setInt(3, desconto);
+                state.setInt(3, 0);
                 state.executeUpdate();
 
                 // Insere os telefones dele na tabela Telefone com base em seu cpf
@@ -77,34 +77,38 @@ public class ControleCupom {
         return null;
     }
 
-    // public static int calculaDesconto(Connection connection, JogoUsuario jogoUsuario, Cupom cupom) {
-    //     PreparedStatement state = null;
+    public static int calculaDesconto(Connection connection, JogoUsuario jogoUsuario, Jogo jogo, Cupom cupom){
+        PreparedStatement state = null;
+        if (buscaCupom(connection, cupom.getIdCupom()) != null && jogoUsuario.getTemCupom() == true) {
+            try {
+                // Insere o usuário na tabela Usuario
+                cupom.setDesconto((jogoUsuario.getQuantObtidaConquistas()/jogo.getQuantConquistas())*jogo.getDescontoElegivel());
+                String query = "UPDATE Into Cupom (desconto) VALUES (?)";
+                state = connection.prepareStatement(query);
+                state.setInt(3, cupom.getDesconto());
+                state.executeUpdate();
 
-    //     if (buscaCupom(connection, cupom.getIdCupom()) != null) {
-    //         try {
+                query = "UPDATE Into jogoUsuario (desconto) VALUES (?)";
+                state = connection.prepareStatement(query);
+                state.setInt(4,0);
+                state.executeUpdate();
 
-    //             // Insere o usuário na tabela Usuario
-    //             cupom.setDesconto(jogoUsuario.getQuantObtidaConquistas() * 10);
-    //             String query = "INSERT Into Cupom (desconto) VALUES (?)";
-    //             state = connection.prepareStatement(query);
-    //             state.setInt(3, cupom.getDesconto());
-    //             state.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (state != null) {
+                        state.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            return cupom.getDesconto();
+        } else{
+            return 0;
+        } 
+    }
 
-    //             // Insere os telefones dele na tabela Telefone com base em seu cpf
-    //             System.out.println("Desconto Cadastrado!");
-
-    //         } catch (SQLException e) {
-    //             e.printStackTrace();
-    //         } finally {
-    //             try {
-    //                 if (state != null) {
-    //                     state.close();
-    //                 }
-    //             } catch (SQLException e) {
-    //                 e.printStackTrace();
-    //             }
-    //         }
-    //     }
-    // }
-    
+       
 }
